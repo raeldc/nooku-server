3b2a313056deb683876441e45d9cee19642af3a6
Johan Janssens <johan@nooku.org>
2010-04-03 20:23:15 +0000

    Restructured trunk to follow new svn layout guidelines. (WIP)
    git-svn-id: http://svn2.assembla.com/svn/nooku-framework/trunk/code/site/components/com_default@1884 3cb9cf8f-ca40-0410-9cbd-9c05aa166901

---
+++ controllers/view.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @version     $Id: koowa.php 1296 2009-10-24 00:15:45Z johan $
+ * @category	Koowa
+ * @package     Koowa_Components
+ * @subpackage  Default
+ * @copyright   Copyright (C) 2007 - 2010 Johan Janssens and Mathias Verraes. All rights reserved.
+ * @license     GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+ * @link        http://www.koowa.org
+ */
+
+/**
+ * Default View Controller
+.*
+ * @author		Johan Janssens <johan@koowa.org>
+ * @category	Koowa
+ * @package     Koowa_Components
+ * @subpackage  Default
+ */
+class ComDefaultControllerView extends KControllerAction
+{
+	/**
+	 * Browse a list of items
+	 *
+	 * @return void
+	 */
+	protected function _actionBrowse()
+	{
+		$model = KFactory::get($this->getModel());
+		if($model->getState()->limit === 0) {
+			$model->set('limit', KFactory::get('lib.joomla.application')->getCfg('list_limit'));
+		}
+			
+		return parent::_actionBrowse();
+	}
+}
